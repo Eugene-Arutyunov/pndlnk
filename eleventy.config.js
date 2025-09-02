beeab6a516e3a0aeec98ec8e691c39fcdc561df4
Eugene Arutyunov <e@bvz.name>
2025-09-02 20:28:14 +0300

    wip

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,21 +1,15 @@
 module.exports = function (conf) {
-  conf.addPassthroughCopy('./src/index.css');
-  conf.addPassthroughCopy('./src/ids/colors.css');
-  conf.addPassthroughCopy('./src/ids/normalize.css');
-  conf.addPassthroughCopy('./src/ids/settings.css');
-  conf.addPassthroughCopy('./src/ids/layout.css');
-  conf.addPassthroughCopy('./src/ids/ids.css');
-  conf.addPassthroughCopy('./src/ids/gallery.css');
-  conf.addPassthroughCopy('./src/ids/photoswipe.css');
-  conf.addPassthroughCopy('./src/index.js');
-  conf.addPassthroughCopy('./src/assets');
-  conf.addPassthroughCopy('./src/fonts');
+  conf.addPassthroughCopy("./src/index.css");
+  conf.addPassthroughCopy("./src/ids");
+  conf.addPassthroughCopy("./src/index.js");
+  conf.addPassthroughCopy("./src/assets");
+  conf.addPassthroughCopy("./src/fonts");
 
   return {
     dir: {
-      input: './src',
-      includes: './includes'
+      input: "./src",
+      includes: "./includes",
     },
-    htmlTemplateEngine: 'njk'
-  }
-}
+    htmlTemplateEngine: "njk",
+  };
+};
